9162e290fe427a66601b34ebd462d7c91b52127e
HaenooKim <68438969+HaenooKim@users.noreply.github.com>
2021-01-15 03:41:28 +0900

    scroll section

--- main.js
+++ main.js
@@ -13,23 +13,38 @@ function handle_Toggle_Click() {
 toggle_Btn.addEventListener('click', handle_Toggle_Click); //여기서 주의점! 절대 handleClick()이라고 쓰지 말 것!
 
 
-// -----------------Navbar Scroll---------------
+// -----------------Navbar is fixed when scrolling---------------
 const navbar = document.querySelector('#navbar');
 // getBoundingClientRect()로 위치값을 가져올 수 있다.
 const navbarHeight = navbar.getBoundingClientRect().height;
 
 function handle_navbar_Scroll() {
-    console.log(window.scrollY);
-    console.log(`Height : ${navbarHeight}`);
+    //window.scrollY를 하면 y축으로 이동한 좌표를 볼 수 있다. 
+    //console.log(window.scrollY);
+    //console.log(`Height : ${navbarHeight}`);
     if (window.scrollY > navbarHeight) {
         navbar.classList.add('navbar__dark');
         navbar_menu.classList.remove('active__menu');
     } else {
         navbar.classList.remove('navbar__dark');
     }
-    
 }
 
 document.addEventListener('scroll', handle_navbar_Scroll);
 
 
+// -----------------Scroll to section---------------
+const navbar_scroll_section = document.querySelector('.navbar__menu');
+
+function handle_item_click(event) {
+    //dataset은 html에서 정의한 data-link="~"때문에 이렇게 적는다.
+    console.log(event.target.dataset.link); //navbar 메뉴 아이템을 클릭할 때 이벤트 타겟이 출력되는 것을 볼 수 있다.
+    const target = event.target;
+    const link = target.dataset.link;
+
+    //참고 : https://developer.mozilla.org/ko/docs/Web/API/Element/scrollIntoView
+    const scrollTo = document.querySelector(link); //link요소를 가져온다.
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
+
+navbar_scroll_section.addEventListener('click', handle_item_click);
